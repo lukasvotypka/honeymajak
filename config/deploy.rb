36f8b2b5ef9bf13a4188a0e12aaf835c2e690dce
Lukas <lukas.votypka@gmail.com>
2010-07-14 20:22:24 +0200

    oprava udajov k databaze

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+set :application, "honeymajak"
+set :repository, "git://github.com/karmi/workshop_tutorial_application.git"
+set :scm, "git"
+
+role :web, "server5.railshosting.cz"
+role :app, "server5.railshosting.cz"
+role :db,  "server5.railshosting.cz", :primary => true
+
+set :deploy_to, "/home/honeymajak/app/"
+set :user, "honeymajak"
+
+set :use_sudo, false
+
+task :after_update_code, :roles => [:app, :db] do
+  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+end
+
+
+namespace :deploy do
+  task :start, :roles => :app do
+  end
+end
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
