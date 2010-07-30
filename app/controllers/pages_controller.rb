313a8aa624aa30da0b1d7486aa1bd25bb9f5cd65
Lukas <lukas.votypka@gmail.com>
2010-07-31 01:53:37 +0200

    opravy title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,25 +1,30 @@
 class PagesController < ApplicationController
   
   def acacia_honey
-
+    @title = t("Acacia Honey")
     respond_to do |format|
       format.html
     end
   end
 
   def honey
+    @title = t("honey")
   end
 
   def honey_wine
+    @title = t("honey_wine")
   end
 
   def about_us
+    @title = t("about_us")
   end
 
   def contact
+    @title = t("contact")
   end
 
   def index
+    @title = t("home")
     @slogan = t("the sweetness of a family business approach")
   end
 
